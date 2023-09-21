408331df2d916bb85996d3fb29114178abd29373
Lyle-Tafoya <lyle.tafoya@gmail.com>
2023-09-21 14:01:08 -0700

    Added missing '#include <array>'

--- src/command2.cpp
+++ src/command2.cpp
@@ -24,6 +24,7 @@ Omega. If not, see <https://www.gnu.org/licenses/>.
 #include "interactive_menu.hpp"
 
 #include <algorithm>
+#include <array>
 #include <chrono>
 #include <format>
 #include <list>
