d67cf57dabbd0c2fecb1d659f549227b495f1199
Lyle-Tafoya <lyle.tafoya@gmail.com>
2023-09-21 22:39:51 -0700

    Version bump to 0.4.1

--- src/date.h
+++ src/date.h
@@ -21,6 +21,6 @@ Omega. If not, see <https://www.gnu.org/licenses/>.
 #ifndef OMEGA_DATE_H_
 #define OMEGA_DATE_H_
 
-#define LAST_OMEGA_EDIT_DATE "Wed Sep 21 01:18 PST 2023"
+#define LAST_OMEGA_EDIT_DATE "Wed Sep 21 22:39 PST 2023"
 
 #endif
