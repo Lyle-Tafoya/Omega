e7a701798c0d26dc414af0825824ac99eaf8a6c4
Lyle-Tafoya <lyle.tafoya@gmail.com>
2023-11-25 23:19:40 -0800

    Added missing copyright and license notice to some files

--- src/monster.cpp
+++ src/monster.cpp
@@ -1,3 +1,21 @@
+/*
+Omega copyright (C) by Laurence Raphael Brothers, 1987,1988,1989
+Modifications copyright (C) by Lyle Tafoya, 2019, 2021-2023
+
+This file is part of Omega.
+
+Omega is free software: you can redistribute it and/or modify it under the terms
+of the GNU General Public License as published by the Free Software Foundation,
+either version 3 of the License, or (at your option) any later version.
+
+Omega is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+Omega. If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "monster.h"
 
 monster::monster(unsigned char attacked, int aux1, int aux2, int x, int y, int click, int id, int hp, int hit, int ac, int dmg, int sense, int wakeup, int level, int speed, unsigned char sleep, unsigned char treasure, long xpv, int corpseweight, int corpsevalue, int transformid, int startthing, unsigned char uniqueness, int talkf, int movef, int meleef, int strikef,int specialf, long status, long immunity, chtype monchar, const std::string &monstring, const std::string &corpsestr, const std::string &meleestr)
