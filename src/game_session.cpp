c18e7edde84f0f724ef524b0eaf37018c25b9772
Lyle-Tafoya <lyle.tafoya@gmail.com>
2026-09-23 12:42:58 -0700

    Fix the typo again

--- src/game_session.cpp
+++ src/game_session.cpp
@@ -186,7 +186,7 @@ InitResult init_game_session()
     }
     else if(Lunarity == -1)
     {
-      queue_message("The feel enervated by the moon!");
+      queue_message("You feel enervated by the moon!");
     }
   }
 
